a509c061402b92b2cf0b08b4cec8c17f1297b97d
673677097 <673677097@qq.com>
2017-10-18 23:08:33 +0800

    velocity macro add

--- velocity-spring-boot/src/main/java/yyh/learn/spring/boot/web/HelloWorldController.java
+++ velocity-spring-boot/src/main/java/yyh/learn/spring/boot/web/HelloWorldController.java
@@ -4,8 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @Controller
 public class HelloWorldController {
+    @RequestMapping("/")
+    public String index(String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
 
     @RequestMapping("/hello")
     public String hello(String name, Model model) {
