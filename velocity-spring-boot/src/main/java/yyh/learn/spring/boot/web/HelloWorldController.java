9280a25d08b4ec615d22f7da17f439edb1a9186e
673677097 <673677097@qq.com>
2017-10-19 22:12:52 +0800

    âadd vm macroâ

--- velocity-spring-boot/src/main/java/yyh/learn/spring/boot/web/HelloWorldController.java
+++ velocity-spring-boot/src/main/java/yyh/learn/spring/boot/web/HelloWorldController.java
@@ -3,7 +3,6 @@ package yyh.learn.spring.boot.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HelloWorldController {
